a6dbc135fa4a80c4bd4cc2022b47a0c5de4a9530
Myat Khant <90891109+VictorKhant@users.noreply.github.com>
2025-11-03 03:27:14 -0800

    Time: 0 ms (100%), Space: 17.9 MB (34.05%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        best = [nums[0], max(nums[0], nums[1])]
+
+        for i in range(2, len(nums)):
+            best.append(max(best[-1],best[-2] + nums[i]))
+        return max(best[-1], best[-2])
